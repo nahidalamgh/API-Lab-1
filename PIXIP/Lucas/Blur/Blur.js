996885ee3b4d07410e8602d2ab4fc1f3ca47ff8a
lucaskasa <60603122+lucaskasa@users.noreply.github.com>
2020-03-11 11:47:08 +0100

    Update Blur.js

--- PIXIP/Lucas/Blur/Blur.js
+++ PIXIP/Lucas/Blur/Blur.js
@@ -1,28 +1,22 @@
 const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-const bg = PIXI.Sprite.from('https://pixijs.io/examples/examples/assets/pixi-filters/bg_depth_blur.jpg');
-bg.width = app.screen.width;
-bg.height = app.screen.height;
-app.stage.addChild(bg);
-
-const littleDudes = PIXI.Sprite.from('https://pixijs.io/examples/examples/assets/pixi-filters/depth_blur_dudes.jpg');
-littleDudes.x = (app.screen.width / 2) - 315;
-littleDudes.y = 200;
+//Creating the image and than making it blury. 
+const littleDudes = PIXI.Sprite.from('Blur2.png');
+littleDudes.x = (app.screen.width / 2) - 200;
+littleDudes.y = 10;
 app.stage.addChild(littleDudes);
 
-const littleRobot = PIXI.Sprite.from('https://pixijs.io/examples/examples/assets/pixi-filters/depth_blur_moby.jpg');
-littleRobot.x = (app.screen.width / 2) - 200;
-littleRobot.y = 100;
-app.stage.addChild(littleRobot);
-
+//Blurfilter for the images.
 const blurFilter1 = new PIXI.filters.BlurFilter();
 const blurFilter2 = new PIXI.filters.BlurFilter();
 
+//Implementing the blur on the images.
 littleDudes.filters = [blurFilter1];
 littleRobot.filters = [blurFilter2];
 
 let count = 0;
+//Blur amount.
 
 app.ticker.add(() => {
     count += 0.005;
